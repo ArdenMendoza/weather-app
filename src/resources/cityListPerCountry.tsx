9dfc2f5dad17723e29d644e5212926052ba2bf20
Arden Mendoza <ardencristophermendoza@gmail.com>
2020-05-12 22:30:51 +0800

    Base react-typescript framework with cities

---
+++ src/resources/cityListPerCountry.tsx
@@ -0,0 +1,18 @@
+export type City = {
+    cityId?: number;
+    cityName: string;
+    country: string;
+}
+
+export const SEACountries: City[] = [
+    { cityId: 1581130, cityName: 'Hanoi', country: 'Vietnam' },
+    { cityId: 1609350, cityName: 'Bangkok', country: 'Thailand' },
+    { cityId: 1701668, cityName: 'Manila', country: 'Philippines' },
+    { cityId: 1733046, cityName: 'Kuala Lumpur', country: 'Malaysia' },
+    { cityId: 1642911, cityName: 'Jakarta', country: 'Indonesia' },
+    { cityId: 1327865, cityName: 'Union of Burma', country: 'Myanmar' },
+    { cityId: 1821306, cityName: 'Phnom Penh', country: 'Cambodia' },
+    { cityId: 1651944, cityName: 'Vientiane', country: 'Laos' },
+    { cityId: 1820814, cityName: 'Brunei Darussalam', country: 'Brunei' },
+    { cityId: 1880252, cityName: 'Singapore', country: 'Singapore' },
+];
